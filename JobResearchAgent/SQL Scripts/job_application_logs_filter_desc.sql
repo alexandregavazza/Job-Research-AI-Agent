18c34159515efaef33e1a2169e5e6b08a2dda57a
alexandregavazza <alexandre_gavazza@yahoo.com.br>
2026-02-21 09:00:35 -0300

    Updated the filter created_at.

--- JobResearchAgent/SQL Scripts/job_application_logs_filter_desc.sql
+++ JobResearchAgent/SQL Scripts/job_application_logs_filter_desc.sql
@@ -1,2 +1,4 @@
 SELECT job_title, company, location, url, created_at
-	FROM public.job_application_logs ORDER BY created_at DESC;
+	FROM public.job_application_logs
+	WHERE created_at::date = CURRENT_DATE
+	ORDER BY created_at DESC;
